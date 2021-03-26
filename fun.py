6ecdd8f45754894c6ee928309ce43cb17dc80eba
silgreen <verdesilvestro@gmail.com>
2021-03-26 22:59:12 +0100

    less strange things in password

--- fun.py
+++ fun.py
@@ -3,5 +3,5 @@ from random import choice, randint
 
 
 def generate(min,max):
-    string_format = ascii_letters + punctuation + digits
+    string_format = ascii_letters + punctuation
     return "".join(choice(string_format) for x in range(randint(min,max)))
